cc4024a49fb7e45cebd5341a611e06865f02318b
yaniccd <yanic.cardin.1@gmail.com>
2023-03-02 01:28:32 +0100

    More tests

--- symbolic_cumulants.py
+++ symbolic_cumulants.py
@@ -4,7 +4,10 @@ statistical properties of gaussian states.
 """
 
 import numpy as np
-from sympy import MatrixSymbol, symbols
+from sympy import MatrixSymbol, symbols, conjugate
+from itertools import product, permutations
+from math import factorial
+from thewalrus.reference import hafnian
 
 # pylint: disable=C0103
 
@@ -63,6 +66,39 @@ def montrealer(A):  # This is technically the "new" montrealer
             (complex): the value of the montrealer
 
     """
+    equation = 0
+    #making the original 2 rows matrix
+    m = int(np.shape(A)[0]/2)
+    original = np.arange(1,2*m+1).reshape(2,m)
+
+    #initial graphs
+    graph1 = list(range(1,m))+[m+1]
+    graph2 = list(range(m+2,2*m+1))+[m]
+
+    #loop over all bistrings and all permutations
+    for bit in bitstrings(m):
+        for perm in permutations(range(1,m)):
+            B =  np.copy(original)
+
+            for i,j in enumerate(bit):
+                if int(j):
+                    buffer = B[0,i]
+                    B[0,i] = B[1,i]
+                    B[1,i] = buffer
+
+            B = B[:,[0]+list(perm)] #first column stays in place always
+
+            #dictionary mapping
+            dico = {j:i+1 for i,j in enumerate(B.reshape(1,2*m)[0])}
+            new_mapping = {dico[i]:dico[j] for i,j in zip(graph1,graph2)}
+
+            term = 1
+            for i,j in new_mapping.items():
+                term *= A[i-1,j-1]
+
+            equation += term
+
+    return equation
 
 
 def laurentienne(M):  # This is technically the "old" montrealer
@@ -74,7 +110,30 @@ def laurentienne(M):  # This is technically the "old" montrealer
     Returns:
             (complex): the value of the laurentienne
     """
-    # If size of matrix equals odd return 0
+    order = len(M)
+    if order % 2 != 0:
+        return 0
+    indices = list(range(order))
+    part = partition(indices)
+    laurent = 0
+    for p in part:  # Ex p = [[1,3],[2]]
+        # Check there are no partitions of odd length parts.
+        check_weight = True
+        for i in p:
+            if len(i) % 2 != 0:
+                check_weight = False
+                break
+        if check_weight:
+            size = len(p) - 1
+            cum = factorial(size) * (-1) ** size  # prefactor
+            for b in p:  # Ex b = [1,3]
+                local_M = M[b][:, b]
+                haf = hafnian(local_M)
+                cum *= haf * conjugate(haf)
+
+            laurent += cum
+
+    return laurent
 
 
 def lavalois(N):
@@ -151,6 +210,7 @@ def hermitian_N(n, initial_index=0):
     N_matrix = MatrixSymbol("N", n + initial_index, n + initial_index)
     return (np.triu(N_matrix, 0) + np.triu(N_matrix, 1).T.conj())[initial_index:, initial_index:]
 
+
 def diagonal_N(n, initial_index=0):
     """
     Returns a diagonal symbolic matrix N of size n.
@@ -163,3 +223,40 @@ def diagonal_N(n, initial_index=0):
         (numpy.ndarray) : symmetric symbolic matix of size n.
     """
     return np.diag(symbols("n"+str(initial_index)+":%d" % (n + initial_index)))
+
+
+def bitstrings(n):
+    """
+    Returns the bistrings from 0 to n/2
+
+    Args:
+        n (int) : Twice the highest bitstring value.
+
+    Returns:
+        (iterable) : An iterable of all bistrings.
+    """
+    for binary in map(''.join, product('01', repeat=n-1)):
+        yield '0'+binary
+
+
+def partition(collection):
+    """
+    Returns the partition of all element of a given collection.
+
+    Args:
+        collection (iterable) : A  collection of elements to be partitioned.
+
+    Returns:
+        (iterable) : An iterable of all the partitions made from the collection
+    """
+    if len(collection) == 1:
+        yield [collection]
+        return
+
+    first = collection[0]
+    for smaller in partition(collection[1:]):
+        # insert `first` in each of the subpartition's subsets
+        for n, subset in enumerate(smaller):
+            yield smaller[:n] + [[first] + subset] + smaller[n + 1 :]
+        # put `first` in its own subset
+        yield [[first]] + smaller
